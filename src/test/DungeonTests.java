9c08a738cf0fb1f97ed8c56202bbeaaf229a6377
Ravija Maheshwari <iamravija@yahoo.com>
2019-07-15 16:43:18 +1000

    Junit starter

---
+++ src/test/DungeonTests.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class DungeonTests {
+		
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception {
+		
+	}
+	
+	@Test
+	void testPlayerMovesUp() {
+		//fail("Not yet implemented");
+		
+	}
+
+}
