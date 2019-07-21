c4f29c4030c5073a1048ee885dc726d34d1f0933
Ravija Maheshwari <iamravija@yahoo.com>
2019-07-21 10:27:31 +1000

    Removed some comments in Player

--- src/unsw/dungeon/Player.java
+++ src/unsw/dungeon/Player.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 /**
  * The player entity
- * @author Robert Clifton-Everest
  *
  */
 public class Player extends Entity {
@@ -66,7 +65,6 @@ public class Player extends Entity {
 
 	@Override
 	public boolean stepOver() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
